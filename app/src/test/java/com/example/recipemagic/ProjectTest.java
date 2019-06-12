211dffef918d5d7c2b37db303701990bb334a68c
akelly96 <kel15011@byui.edu>
2019-06-12 09:07:35 -0600

    Added Test functions

---
+++ app/src/test/java/com/example/recipemagic/ProjectTest.java
@@ -0,0 +1,21 @@
+package com.example.recipemagic;
+
+import org.junit.Test;
+
+public class ProjectTest {
+
+    @Test
+    public void something1(){
+        
+    }
+
+    @Test
+    public void something2(){
+
+    }
+
+    @Test
+    public void something3(){
+
+    }
+}
