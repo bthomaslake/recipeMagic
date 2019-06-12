113449330ff9d8e12fe6727bd0ef2f64331e4331
tlake29 <tlake29@gmail.com>
2019-06-12 10:04:26 -0600

    Added cookBook test code

--- app/src/test/java/com/example/recipemagic/CookBookTest.java
+++ app/src/test/java/com/example/recipemagic/CookBookTest.java
@@ -1,4 +1,17 @@
 package com.example.recipemagic;
 
+import com.example.recipemagic.model.cookBook;
+import com.example.recipemagic.presenter.MainPresenter;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertNotNull;
+
 public class CookBookTest {
+    @Test
+    public void cookBookValid(){
+        MainPresenter presenter = new MainPresenter();
+        cookBook book = presenter.getCookBook();
+        assertNotNull("Book exists", book);
+    }
 }
