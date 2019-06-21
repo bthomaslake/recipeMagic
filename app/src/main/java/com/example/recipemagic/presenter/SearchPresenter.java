39d372c0e001141c73ccf1c95a1aacc24730b6d2
tlake29 <tlake29@gmail.com>
2019-06-21 08:42:52 -0600

    quickie update on presenters

--- app/src/main/java/com/example/recipemagic/presenter/SearchPresenter.java
+++ app/src/main/java/com/example/recipemagic/presenter/SearchPresenter.java
@@ -1,7 +1,13 @@
 package com.example.recipemagic.presenter;
 
+import com.example.recipemagic.model.cookBook;
+
 public class SearchPresenter {
     public SearchPresenter(MainPresenter presenter) {
+        private cookBook book;
+    public RecipeListPresenter(MainPresenter presenter) {
+            book = presenter.getCookBook();
 
+        }
     }
 }
