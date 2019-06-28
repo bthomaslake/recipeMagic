5de506bed7151b512cca6e87d51168bc4e407943
Jeremy Duong <duo13001@byui.edu>
2019-06-28 08:56:44 -0600

    worked on search

--- app/src/main/java/com/example/recipemagic/presenter/SearchPresenter.java
+++ app/src/main/java/com/example/recipemagic/presenter/SearchPresenter.java
@@ -10,17 +10,23 @@ import java.util.List;
 public class SearchPresenter {
     private CookBook book;
 
-    private List<RecipeHelper> category;
+    private List<RecipeHelper> recipes;
 
     public SearchPresenter(MainPresenter presenter) {
         book = presenter.getCookBook();
-        category = new ArrayList<RecipeHelper>();
     }
 
     public void searchTerm(String term) {
-        category.clear();
+        recipes.clear();
 
         //This is where you do the loop, search for ingredients in: each category -> recipe.getIngredientString()
+        for (List<RecipeHelper> list_rh : book.getRecipes()) {
+            for (RecipeHelper rh : list_rh) {
+                if (rh.getRecipeIngredients().contains(term)) {
+                    recipes.add(rh);
+                }
 
+            }
+        }
     }
 }
