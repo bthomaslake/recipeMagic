b4840b24ca11b45156ce228dfba98afbd79c1c85
Mason Grant <gumbymason@gmail.com>
2019-06-29 19:34:39 -0600

    Basic working timer

--- app/src/main/java/com/example/recipemagic/view/Timer.java
+++ app/src/main/java/com/example/recipemagic/view/Timer.java
@@ -1,182 +1,129 @@
 package com.example.recipemagic.view;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.os.CountDownTimer;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.recipemagic.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Timer.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Timer#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Timer extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.util.Locale;
 
-    private TextView timerDisplay;
-    private Button startButton;
-
-    private CountDownTimer countDownTimer;
-    private long timeLeftInMilliSeconds = 600000;
-    boolean timerRunning;
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
+public class Timer extends Fragment implements View.OnClickListener {
     public Timer() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Timer.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Timer newInstance(String param1, String param2) {
-        Timer fragment = new Timer();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private static final long START_TIME_IN_MILLIS = 600000;
 
-    @Override
-    public void  onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    private TextView mTextViewCountDown;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+
+    private CountDownTimer mCountDownTimer;
+
+    private boolean mTimerRunning;
+    View layoutView;
+
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        layoutView = inflater.inflate(R.layout.fragment_timer, container, false);
 
 
-    }
+        mTextViewCountDown = layoutView.findViewById(R.id.timerDisplay);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_timer,container, false);
+        mButtonStartPause = layoutView.findViewById(R.id.start_button);
+        mButtonReset = layoutView.findViewById(R.id.restart_button);
 
-        timerDisplay = view.findViewById(R.id.timerDisplay);
-        startButton = view.findViewById(R.id.start_button);
-
-        startButton.setOnClickListener(new View.OnClickListener(){
+        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                startStop();
+            public void onClick(View v) {
+                if (mTimerRunning) {
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
             }
         });
 
-        return inflater.inflate(R.layout.fragment_timer, container, false);
-    }
-
-    public void startStop(){
-        if (timerRunning){
-            stopTimer();
-        }
-        else{
-            startTimer();
-        }
-    }
-
-    public void startTimer(){
-        countDownTimer = new CountDownTimer(timeLeftInMilliSeconds, 1000){
+        mButtonReset.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTick(long l){
-                timeLeftInMilliSeconds = l;
-                updateTimer();
+            public void onClick(View v) {
+                resetTimer();
+            }
+        });
+
+        updateCountDownText();
+        return layoutView;
+    }
+    private void startTimer() {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
             }
 
             @Override
             public void onFinish() {
-
+                mTimerRunning = false;
+                mButtonStartPause.setText("Start");
+                mButtonStartPause.setVisibility(View.INVISIBLE);
+                mButtonReset.setVisibility(View.VISIBLE);
             }
         }.start();
-        startButton.setText("PAUSE");
-        timerRunning = true;
+
+        mTimerRunning = true;
+        mButtonStartPause.setText("Pause");
+        mButtonReset.setVisibility(View.INVISIBLE);
     }
 
-    public void stopTimer() {
-        countDownTimer.cancel();
-        startButton.setText("START");
-        timerRunning = false;
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mButtonStartPause.setText("Start");
+        mButtonReset.setVisibility(View.VISIBLE);
     }
 
-    public void updateTimer() {
-        int minutes = (int) timeLeftInMilliSeconds / 60000;
-        int seconds = (int) timeLeftInMilliSeconds % 60000 / 1000;
-
-        String timeLeftText;
-
-        timeLeftText = "" + minutes;
-        timeLeftText += ":";
-        if(seconds < 10) timeLeftText += "0";
-        timeLeftText += seconds;
-
-        timerDisplay.setText(timeLeftText);
+    private void resetTimer() {
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        updateCountDownText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+        mButtonStartPause.setVisibility(View.VISIBLE);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+    private void updateCountDownText() {
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        mTextViewCountDown.setText(timeLeftFormatted);
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void onClick(View v) {
+        switch (layoutView.getId()) {
+            case R.id.start_button:
+                if (mTimerRunning) {
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+                break;
+            case R.id.restart_button:
+                resetTimer();
+                break;
+
+        }
+        updateCountDownText();
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
