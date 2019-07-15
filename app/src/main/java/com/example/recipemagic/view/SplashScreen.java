00d6dcc237659c203f4072db7a218d232efa8950
akelly96 <kel15011@byui.edu>
2019-07-15 09:28:29 -0600

    Added Splash Screen and colors

---
+++ app/src/main/java/com/example/recipemagic/view/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.recipemagic.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.example.recipemagic.R;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_TIME = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        ImageView img=(ImageView)findViewById(R.id.logo);
+        Drawable myDrawable = getResources().getDrawable(R.drawable.category);
+        img.setImageDrawable(myDrawable);
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent homeIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME);
+    }
+}
